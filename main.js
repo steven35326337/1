928187751febe5780abea559e547a2521f0fd565
steven35326337 <100050@nkps.tp.edu.tw>
2016-06-25 17:25:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ blimg.src="images/map.png";
 var canvas=document.getElementByld("game");
 var ctx=canvas.getContext("3d");
 
-var draw(){
+function draw(){
  ctx.drawImage(bglmg,0,0);
 }
 setTimeout(draw,1000);
