475cba97da7a67716bbe0817a0d1793ad4923085
steven35326337 <100050@nkps.tp.edu.tw>
2016-06-25 17:52:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
+var hero=document.createElement("img");
+bglmg.src="images/map.png";
 var canvas=document.getElementById("game");
 var ctx=canvas.getContext("2d");
 
