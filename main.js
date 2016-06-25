5f69505497feb61dd464a0aabc825c42fc799fb5
steven35326337 <100050@nkps.tp.edu.tw>
2016-06-25 17:54:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
 var hero=document.createElement("img");
-bglmg.src="images/map.png";
+bglmg.src="images/rukia.gif";
 var canvas=document.getElementById("game");
 var ctx=canvas.getContext("2d");
 
 function draw(){
  ctx.drawImage(bglmg,0,0);
+ ctx.drawImage(hero,0,0);
 }
 setTimeout(draw,1000);
